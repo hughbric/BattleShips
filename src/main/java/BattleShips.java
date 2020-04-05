d79b7b4da332e9cff29ff6e6822a9421c28783b1
Michael Hughes <mhughes36@live.co.uk>
2020-04-05 22:28:47 +0100

    a player can deploy boats based off of the XY positions that they input

--- src/main/java/BattleShips.java
+++ src/main/java/BattleShips.java
@@ -1,24 +1,47 @@
+import java.util.Scanner;
+
 public class BattleShips {
-    static String[][] board = new String[10][10];
+    static String[][] playerBoard = new String[10][10];
+    static Integer[][] playerBoatPositions = new Integer[5][2];
+
     public static void main(String[] args) {
         System.out.println("**** Welcome to Battle Ships game ****\n");
         System.out.println("Right now, the ocean is empty.\n");
         DrawBoard();
+        SetBoatPositions();
+        DeployShips(playerBoard, playerBoatPositions);
+        DrawBoard();
     }
 
-    private static void DrawBoard() {
+    static void DrawBoard() {
         System.out.println("  0123456789");
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 0; i < playerBoard.length; i++) {
             System.out.print(i + "|");
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == null) {
+            for (int j = 0; j < playerBoard.length; j++) {
+                if (playerBoard[i][j] == null) {
                     System.out.print(" ");
                 } else {
-                    System.out.print(board[i][j]);
+                    System.out.print(playerBoard[i][j]);
                 }
             }
             System.out.println("|" + i);
         }
         System.out.println("  0123456789");
+    }
+
+    static void SetBoatPositions() {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter X coordinate for your ship: ");
+            playerBoatPositions[i][0] = input.nextInt();
+            System.out.println("Enter Y coordinate for your ship: ");
+            playerBoatPositions[i][1] = input.nextInt();
+        }
+    }
+
+    static void DeployShips(String[][] board, Integer[][] boatPositions) {
+        for (int i = 0; i < 5; i++) {
+            board[boatPositions[i][0]][boatPositions[i][1]] = "@";
+        }
     }
 }
--- src/test/java/BattleShipsTest.java
+++ src/test/java/BattleShipsTest.java
@@ -24,13 +24,9 @@ public class BattleShipsTest {
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void testInitialisesGameBoard() {
-        BattleShips.main(new String[] {});
+    public void testDrawsBoard() {
+        BattleShips.DrawBoard();
         assertEquals(
-                "**** Welcome to Battle Ships game ****\n" +
-                        "\n" +
-                        "Right now, the ocean is empty.\n" +
-                        "\n" +
                         "  0123456789\n" +
                         "0|          |0\n" +
                         "1|          |1\n" +
@@ -44,5 +40,19 @@ public class BattleShipsTest {
                         "9|          |9\n" +
                         "  0123456789\n",
                 outContent.toString());
+    }
+
+    @Test
+    public void testShipsGetDeployedOnBoard() {
+        String[][] board = new String[10][10];
+        Integer[][] boats = {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}};
+
+        BattleShips.DeployShips(board, boats);
+
+        assertEquals(board[0][0], "@");
+        assertEquals(board[1][1], "@");
+        assertEquals(board[2][2], "@");
+        assertEquals(board[3][3], "@");
+        assertEquals(board[4][4], "@");
     }
 }
