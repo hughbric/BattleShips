02ae22a4b4180f7ed7329e9c9fd25f4727d5e11f
Michael Hughes <mhughes36@live.co.uk>
2020-04-05 14:35:57 +0100

    initialise the play board

--- src/main/java/BattleShips.java
+++ src/main/java/BattleShips.java
@@ -1,11 +1,24 @@
 public class BattleShips {
+    static String[][] board = new String[10][10];
     public static void main(String[] args) {
-        BattleShips game = new BattleShips();
-        game.play();
+        System.out.println("**** Welcome to Battle Ships game ****\n");
+        System.out.println("Right now, the ocean is empty.\n");
+        DrawBoard();
     }
 
-    public void play() {
-        System.out.println("**** Welcome to Battle Ships game ****\n");
-        System.out.println("Right now, the ocean is empty.");
+    private static void DrawBoard() {
+        System.out.println("  0123456789");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(i + "|");
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == null) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(board[i][j]);
+                }
+            }
+            System.out.println("|" + i);
+        }
+        System.out.println("  0123456789");
     }
 }
--- src/test/java/BattleShipsTest.java
+++ src/test/java/BattleShipsTest.java
@@ -1,16 +1,48 @@
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ExpectedException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class BattleShipsTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void testGameInitialises() {
-        assertEquals(0, new BattleShips());
+    public void testInitialisesGameBoard() {
+        BattleShips.main(new String[] {});
+        assertEquals(
+                "**** Welcome to Battle Ships game ****\n" +
+                        "\n" +
+                        "Right now, the ocean is empty.\n" +
+                        "\n" +
+                        "  0123456789\n" +
+                        "0|          |0\n" +
+                        "1|          |1\n" +
+                        "2|          |2\n" +
+                        "3|          |3\n" +
+                        "4|          |4\n" +
+                        "5|          |5\n" +
+                        "6|          |6\n" +
+                        "7|          |7\n" +
+                        "8|          |8\n" +
+                        "9|          |9\n" +
+                        "  0123456789\n",
+                outContent.toString());
     }
 }
